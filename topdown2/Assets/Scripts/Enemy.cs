b97669e31bf53e0f0bab1bc7bb9c4b6a5336f112
ITHUB\Teacher <312>
2024-10-16 17:16:50 +0300

    top down controller
    yes

---
+++ topdown2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Rigidbody2D))]
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    //[SerializeField] private float minFollowDistance;
+    private Rigidbody2D _rb;
+    private Transform _followtarget;
+
+    private bool _isCollided;
+
+    
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out TopDownController _))
+        {
+            _isCollided = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out TopDownController _))
+        {
+            _isCollided = false;
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out TopDownController _))
+        {
+            _followtarget = collision.transform;
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        //float distance = Vector2.Distance(transform.position, _followtarget.position);
+        if(_followtarget && ! _isCollided)
+        {
+            _rb.MovePosition(transform.position - _followtarget.position * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out TopDownController player))
+        {
+            _followtarget = null;
+        }
+    }
+}

9a9ba090b8ea4edcdadce57d41b2d44e9c7a8b58
ITHUB\Teacher <312>
2024-10-16 15:47:56 +0300

    added topdowncontroller
    added topdowncontroller

---
+++ topdown2/Assets/Scripts/TopDownController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class TopDownController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+        _rb.velocity = moveDirection * speed;
+    }
+}

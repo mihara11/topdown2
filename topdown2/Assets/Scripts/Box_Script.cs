248b29e0733af4690d67c0867ab7736634d963d2
ITHUB\Teacher <312>
2024-11-06 17:12:31 +0300

    minor changes

--- topdown2/Assets/Scripts/Box_Script.cs
+++ topdown2/Assets/Scripts/Box_Script.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Box_Script : MonoBehaviour
 {
-    public int health = 5; // Количество очков здоровья коробки
-    public GameObject splinterPrefab; // Префаб щепки
-    public int splinterCount = 5; // Количество щепок при разрушении коробки
-    public float splinterForce = 5f; // Сила разлета щепок
+    public int health = 5; 
+    public GameObject splinterPrefab; 
+    public int splinterCount = 5; 
+    public float splinterForce = 5f; 
 
-    // Метод, вызываемый при попадании по коробке
+    
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -21,34 +21,34 @@ public class Box_Script : MonoBehaviour
         }
     }
 
-    // Метод для разрушения коробки и создания щепок
+   
     private void DestroyBox()
     {
         for (int i = 0; i < splinterCount; i++)
         {
-            // Создаем щепку
+          
             GameObject splinter = Instantiate(splinterPrefab, transform.position, Random.rotation);
-            // Получаем Rigidbody щепки для добавления силы разлета
+            
             Rigidbody rb = splinter.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                // Применяем силу разлета к щепке
+                
                 Vector3 randomDirection = Random.insideUnitSphere.normalized;
                 rb.AddForce(randomDirection * splinterForce, ForceMode.Impulse);
             }
         }
 
-        // Уничтожаем объект коробки
+      
         Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Проверка попадания по коробке пулей (на примере объекта с тегом "Bullet")
+       
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            TakeDamage(1); // Отнимаем 1 HP при попадании пули
-            Destroy(collision.gameObject); // Уничтожаем пулю
+            TakeDamage(1);
+            Destroy(collision.gameObject); 
             Debug.Log("-1");
         }
     }

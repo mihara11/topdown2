66e886f11cc580efeaf5c213c12f5297a4567e8d
ITHUB\Student <qew>
2024-11-20 18:05:03 +0300

    minor changes

--- topdown2/Assets/Scripts/TopDownController.cs
+++ topdown2/Assets/Scripts/TopDownController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Scripting.APIUpdating;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class TopDownController : MonoBehaviour
